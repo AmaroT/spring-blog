1f6d8d81d824ea8818211b91189e93b144e35c36
Amaro Terrazas <42896111+AmaroT@users.noreply.github.com>
2020-10-05 11:01:19 -0500

    init commit

---
+++ src/main/java/com/codeup/blog/controllers/IncrementController.java
@@ -0,0 +1,14 @@
+package com.codeup.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+class IncrementController {
+
+    @RequestMapping(path = "increment/{number}", method = RequestMethod.GET)
+    @ResponseBody
+    public String addOne(@PathVariable int number) {
+        return number + " plus one is " + (number + 1) + "!";
+    }
+}
