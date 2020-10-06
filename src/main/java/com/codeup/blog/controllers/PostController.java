ce070fdbec7040a62d53a06e47452c70d9e7695a
Amaro Terrazas <42896111+AmaroT@users.noreply.github.com>
2020-10-06 15:37:26 -0500

    completed controller exercise

--- src/main/java/com/codeup/blog/controllers/PostController.java
+++ src/main/java/com/codeup/blog/controllers/PostController.java
@@ -1,4 +1,37 @@
 package com.codeup.blog.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class PostController {
+
+    @GetMapping("/posts")
+    @ResponseBody
+    public String getPost() {
+        return "<h1>Posts Index Page</h1>";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String getPostById(@PathVariable long id) {
+        return "Showing individual post: " + id;
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String createPost() {
+        return "View the form for creating a post";
+    }
+
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String submitCreatePost() {
+        return "create a new post";
+    }
+
+
 }
