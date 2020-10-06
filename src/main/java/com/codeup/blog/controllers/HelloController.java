413f0250afdf53bc05b55e0a84509933e5652ed8
Amaro Terrazas <42896111+AmaroT@users.noreply.github.com>
2020-10-06 15:20:15 -0500

    init exercise, fixed issue

--- src/main/java/com/codeup/blog/controllers/HelloController.java
+++ src/main/java/com/codeup/blog/controllers/HelloController.java
@@ -15,9 +15,4 @@ class HelloController {
     public String sayHello(@PathVariable String name) {
         return "Hello, " + name + "!";
     }
-    @RequestMapping(path = "increment/{number}", method = RequestMethod.GET)
-    @ResponseBody
-    public String addOne(@PathVariable int number) {
-        return number + " plus one is " + (number + 1) + "!";
-    }
 }
